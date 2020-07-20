d843d0e70d1419733c867e8b91fde088cfc6f91a
kwl <kwl@localhost.localdomain>
2020-07-20 03:08:28 +0000

    孔维龙

---
+++ app/Http/Controllers/Text/TextController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Text;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TextController extends Controller
+{
+    public function yangxiaokai(){
+        echo "kwl";
+    }
+}
